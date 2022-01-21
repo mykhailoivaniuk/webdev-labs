08d8d9d872751db5e2956682d13034f20cde2275
Misha Ivaniuk <mykhailo.ivaniuk@gmail.com>
2022-01-21 17:03:26 -0600

    initial commit

---
+++ lab03/answers.sql
@@ -0,0 +1,71 @@
+-- Exercise 1 (done for you):
+SELECT * FROM users;
+
+
+
+-- Exercise 2 (done for you):
+SELECT id, first_name, last_name 
+FROM users;
+
+
+
+-- Exercise 3
+
+Select id, first_name,last_name 
+FROM users
+Order BY last_name;
+
+
+-- Exercise 4
+Select id, image_url, user_id 
+from posts 
+Where user_id=26;
+
+
+
+-- Exercise 5
+
+
+
+
+-- Exercise 6
+
+
+
+
+-- Exercise 7
+
+
+
+
+-- Exercise 8
+
+
+
+
+-- Exercise 9
+
+
+
+
+-- Exercise 10
+
+
+
+
+-- Exercise 11
+
+
+
+
+-- Exercise 12
+
+
+
+
+-- Exercise 13
+
+
+
+
+-- Exercise 14
